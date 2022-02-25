f44d43ce91bcbf8936093efe46a1ec58d7acce54
Victor Kingi <kingivictor77@gmail.com>
2022-02-25 12:30:53 +0000

    works

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
 module load languages/anaconda2/5.0.1
-module load languages/gcc/9.3.0
+module load languages/intel/2020-u4
