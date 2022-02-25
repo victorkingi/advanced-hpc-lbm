ddf383f378921f54622134f23cebe53399e3cf65
Victor Kingi <kingivictor77@gmail.com>
2022-02-25 11:45:13 +0000

    works

--- test_one.sh
+++ test_one.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-make clean; rm gmon.out; rm profile.txt; make; ./d2q9-bgk input_$1x$1.params obstacles_$1x$1.dat; gprof d2q9-bgk gmon.out > profile.txt
+# make clean; rm gmon.out; rm profile.txt; make; ./d2q9-bgk input_$1x$1.params obstacles_$1x$1.dat; gprof d2q9-bgk gmon.out > profile.txt
+make clean; rm gmon.out; rm profile.txt; make
