b066a6322e62c259bb96a0e3cefd0d16992d427e
Victor Kingi <kingivictor77@gmail.com>
2022-03-09 09:04:02 +0000

    final submission

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -638,6 +638,7 @@ int initialise(const char *paramfile, const char *obstaclefile,
   float w1 = params->density / 9.f;
   float w2 = params->density / 36.f;
 
+  #pragma omp parallel for collapse(2)
   for (int jj = 0; jj < params->ny; jj++)
   {
     for (int ii = 0; ii < params->nx; ii++)
