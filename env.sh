98fce3c3ebeec07c425561d4f35f94fc4dfde7bc
Victor Kingi <kingivictor77@gmail.com>
2022-03-09 09:09:24 +0000

    final submission

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
+export OMP_PROC_BIND=true
 module load languages/anaconda2/5.0.1
 module load languages/intel/2020-u4
