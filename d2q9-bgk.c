4d09d44aadc2942d21550db72c67ee47561af94d
victorkingi <kingivictor77@gmail.com>
2022-03-25 14:03:49 +0000

    change

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -298,7 +298,7 @@ float timestep(const t_param params, t_speed* restrict cells, t_speed* restrict 
   __assume_aligned(tmp_cells->speed_7, 64);
   __assume_aligned(tmp_cells->speed_8, 64);
 
-  #pragma omp simd
+  #pragma omp simd reduction(+:tot_cells, tot_u)
   for (unsigned int jj = 0; jj < params.ny; jj++)
   {
     for (unsigned int ii = 0; ii < params.nx; ii++)
