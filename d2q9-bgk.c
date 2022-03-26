ea0d04fa62b91a8d315da8b2a698661237d42b61
victorkingi <kingivictor77@gmail.com>
2022-03-26 13:37:09 +0000

    change

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -291,12 +291,10 @@ float timestep(const t_param params, t_speed* restrict cells, t_speed* restrict 
     for (int ii = 0; ii < params.nx; ii++)
     {
       __assume((obstacles[jj*params.nx + ii])<2);
-      unsigned int y_n = (is_power_of_2) ? ((jj+1) & (params.ny - 1)) : ((jj+1) % params.ny);
-      unsigned int x_e = (is_power_of_2) ? ((ii+1) & (params.nx - 1)) : ((ii+1) % params.nx);
+      unsigned int y_n = (jj+1 == params.ny) ? 0 : (jj+1);
+      unsigned int x_e = (ii+1 == params.nx) ? 0 : (ii+1);
       unsigned int y_s = (jj == 0) ? (jj + params.ny - 1) : (jj - 1);
       unsigned int x_w = (ii == 0) ? (ii + params.nx - 1) : (ii - 1);
-      printf("case %d %% %d = %d\n", (jj+1), params.ny, y_n);
-      printf("case %d %% %d = %d\n", (ii+1), params.nx, x_e);
       __assume(y_n < 128);
       __assume(x_e < 128);
 
