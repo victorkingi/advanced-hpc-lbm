5f745c68c44ef54f2f09d4543570df194f70a2f6
victorkingi <kingivictor77@gmail.com>
2022-03-26 17:36:22 +0000

    change

--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
-export OMP_PROC_BIND=true
-export OMP_PLACES=cores
+
 module load languages/anaconda2/5.0.1
 module load languages/intel/2020-u4
