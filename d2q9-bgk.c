e3a9e2ab20110c6b4624dbcfb38069160030953a
Victor Kingi <kingivictor77@gmail.com>
2022-04-26 13:37:55 +0100

    done

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -55,6 +55,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include "mpi.h"
 
 #define NSPEEDS 9
 #define FINALSTATEFILE "final_state.dat"
@@ -143,6 +144,45 @@ unsigned int check_power_of_2(unsigned int x) {
 */
 int main(int argc, char *argv[])
 {
+  //MPI INIT
+  int rank;               /* 'rank' of process among it's cohort */ 
+  int size;               /* size of cohort, i.e. num processes started */
+  int flag;               /* for checking whether MPI_Init() has been called */
+  int strlen;             /* length of a character array */
+  enum bool {FALSE,TRUE}; /* enumerated type: false = 0, true = 1 */  
+  char hostname[MPI_MAX_PROCESSOR_NAME];  /* character array to hold hostname running process */
+
+  /* initialise our MPI environment */
+  MPI_Init( &argc, &argv );
+
+  /* check whether the initialisation was successful */
+  MPI_Initialized(&flag);
+  if ( flag != TRUE ) {
+    MPI_Abort(MPI_COMM_WORLD,EXIT_FAILURE);
+  }
+
+  /* determine the hostname */
+  MPI_Get_processor_name(hostname,&strlen);
+
+  /* 
+  ** determine the SIZE of the group of processes associated with
+  ** the 'communicator'.  MPI_COMM_WORLD is the default communicator
+  ** consisting of all the processes in the launched MPI 'job'
+  */
+  MPI_Comm_size( MPI_COMM_WORLD, &size );
+  
+  /* determine the RANK of the current process [0:SIZE-1] */
+  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+
+  /* 
+  ** make use of these values in our print statement
+  ** note that we are assuming that all processes can
+  ** write to the screen
+  */
+  printf("Hello, world; from host %s: process %d of %d\n", hostname, rank, size);
+
+
+
   char *paramfile = NULL;                                                            /* name of the input parameter file */
   char *obstaclefile = NULL;                                                         /* name of a the input obstacle file */
   t_param params;                                                                    /* struct to hold parameter values */
@@ -212,6 +252,9 @@ int main(int argc, char *argv[])
   printf("Elapsed Total time:\t\t\t%.6lf (s)\n", tot_toc - tot_tic);
   write_values(params, cells, obstacles, av_vels);
   finalise(&params, &cells, &tmp_cells, &obstacles, &av_vels);
+
+  /* finialise the MPI enviroment */
+  MPI_Finalize();
 
   return EXIT_SUCCESS;
 }
--- env.sh
+++ env.sh
@@ -2,4 +2,4 @@
 # compile and run to this file.
 
 module load languages/anaconda2/5.0.1
-module load languages/intel/2020-u4
+module add languages/intel/2018-u3
