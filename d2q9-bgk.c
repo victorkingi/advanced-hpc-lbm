81668e1c2cb40bc4bda2e9e86e5ff3b6ed9f08fe
Victor Kingi <kingivictor77@gmail.com>
2022-04-29 18:54:19 +0100

    test

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -100,7 +100,7 @@ typedef struct {
 
 int calc_ncols_from_rank(const t_param params, int rank, int size);
 
-int calc_all_rank_sizes(int size, int ny);
+void calc_all_rank_sizes(int size, int ny, map_rank **ranks);
 
 /* load params, allocate memory, load obstacles & initialise fluid particle densities */
 int initialise(const char *paramfile, const char *obstaclefile,
