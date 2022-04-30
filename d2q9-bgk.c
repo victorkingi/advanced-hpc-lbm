329973f4a073ae20612020bc52761ef7a57594a0
Victor Kingi <kingivictor77@gmail.com>
2022-04-30 02:11:45 +0100

    mpi version functional

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -243,6 +243,7 @@ int main(int argc, char *argv[])
 
   accelerate_flow(params, cells, obstacles);
 
+  #pragma ivdep
   for (int tt = 0; tt < params.maxIters; tt++)
   {
     local_vals = timestep(params, cells, tmp_cells, obstacles, start_col, end_col);
